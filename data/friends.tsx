b118d725a0bc2c8fa2a06912429ed61881abe1af
Brandon <Brandon-LIs@users.noreply.github.com>
2026-08-05 14:53:30 +0800

    friends: 新增博客大联盟友链；友链数据改为 friends.json 便于后台管理

--- data/friends.tsx
+++ data/friends.tsx
@@ -1,23 +1,6 @@
-export const Friends: Friend[] = [
-  {
-    title: '宜昌市第一中学',
-    description: '文脉千年，学在一中。欢迎来到宜昌一中。',
-    website: 'https://www.ycyz.com/',
-    avatar: '/img/friends/ycyz.ico',
-  },
-  {
-    title: '西陵金初',
-    description: '宜昌市西陵区金东方初中官网。',
-    website: 'https://xlcz.jdfschool.com/',
-    avatar: '/img/friends/jdfschool.svg',
-  },
-  {
-    title: "Kazuki's Blog",
-    description: '一名热爱计算机科学与科技创新的高中生。',
-    website: 'https://www.ycxhl.top/',
-    avatar: '/img/friends/ycxhl.svg',
-  },
-]
+import friendsData from './friends.json';
+
+export const Friends: Friend[] = friendsData;
 
 export type Friend = {
   title: string

edca97996826405ff42c8c458f5080d99af0f28d
cnb.cUfyKYAYgJA <QIKYAOrkcHmZuPeGza26aG+cnb.cUfyKYAYgJA@noreply.cnb.cool>
2026-08-12 19:05:05 +0800

    feat: add Bing site verification

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
     { tagName: 'style', attributes: {}, innerHTML: '@import url("https://fontsapi.zeoseven.com/79/main/result.css");@import url("https://fontsapi.zeoseven.com/7/main/result.css");' },
     // 搜索引擎验证
     { tagName: 'meta', attributes: { name: 'sogou_site_verification', content: 'XAWthKRnIS' } },
+    { tagName: 'meta', attributes: { name: 'msvalidate.01', content: 'E4B3D7DAC6638D437E39343DD8E21EE9' } },
     // DNS 预解析 + 预连接外部资源
     { tagName: 'link', attributes: { rel: 'preconnect', href: 'https://jsd.oopss.top' } },
     { tagName: 'link', attributes: { rel: 'preconnect', href: 'https://apis.oopss.top' } },
